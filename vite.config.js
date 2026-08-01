d89dee5b50fe61be1f99c1d90c3b94a6993a81fa
Juan Csv <juanvillalba00216@gmail.com>
2026-07-31 19:25:29 -0500

    Fix build vercel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
-import htmlMinify from 'vite-plugin-html-minify';
 
 export default defineConfig({
-  plugins: [
-    htmlMinify({
-      removeComments: true,
-      collapseWhitespace: true,
-      minifyCSS: true,
-      minifyJS: true
-    })
-  ]
+  build: {
+    minify: 'esbuild'
+  }
 });
