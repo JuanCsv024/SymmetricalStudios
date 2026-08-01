76cf87fa2db8fe0607d7046ede512ddb9db1ac77
Juan Csv <juanvillalba00216@gmail.com>
2026-07-31 19:39:57 -0500

    solucion definitiva vercel

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
-  },
+    minify: true
+  }
 });
